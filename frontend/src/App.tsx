e691029c2ea2b229e420cd7b883a611c08eb1f5f
karthik90100 <karthikkumar7011@gmail.com>
2026-02-10 08:41:43 +0530

    added a new page component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Home from "./pages/Home";
 import MovieDetails from "./pages/MovieDetails";
 import Watchlist from "./pages/Watchlist";
 import Navbar from "./components/Navbar";
+import Page from "./pages/page";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/movie/:id" element={<MovieDetails />} />
           <Route path="/watchlist" element={<Watchlist />} />
+          <Route path="/Page" element={<Page />} />
         </Routes>
       </main>
     </>
---
+++ frontend/src/pages/page.tsx
@@ -0,0 +1,9 @@
+
+function Page() {
+    const uname = "Karthik"
+    return (
+        <h1>{uname == "Karthik" ? "Hello" : false}</h1>
+    )
+}
+
+export default Page;
